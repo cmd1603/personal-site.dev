8a2c531e640274ab8743175362a5e250e48e9668
Chris Davila <cmd1603@gmail.com>
2016-08-22 08:50:37 -0500

    Saving after ansible playbooks run

---
+++ public/js/weather.js
@@ -0,0 +1,95 @@
+$($(document).ready(function() {
+
+    "use strict";
+
+    var geocoder = new google.maps.Geocoder();
+    var cityName = $("#cityname");
+    var mapHolder = $(".map");
+    var enterLocation = $("#enterLocation");
+    var currentLocation = "San Antonio, TX";
+
+    var mapOptions = {
+        zoom: 5,
+        center: {lat: 29.4267904, lng: -98.4917651}
+    };
+    console.log(mapOptions);
+    // Render the map
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: mapOptions.center,
+        map: map,
+        draggable: true
+    });
+
+
+    function geocodeAddress() {
+        geocoder.geocode({"address": city}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.SetCenter(results[0].geometry.location);
+            } else {
+                alert("Geocoding not successful - STATUS: " + status);
+            }
+
+            marker.setPosition(results[0].geometry.location);
+
+            google.maps.event.addListener(marker, "dragend", function (event) {
+                updateForecast(event.latLng);
+            })
+            loadWeather(results[0].geometry.location);
+        })
+    }
+
+
+    function loadWeather(latLng) {
+        var info = {
+            APPID: "0fd75c2b05556c4a57af0d4b517196ec",
+            lat: latLng.lat(),
+            lon: latLng.lng(),
+            units: 'imperial',
+            cnt: 3
+        };
+
+        $.get("http://api.openweathermap.org/data/2.5/forecast/daily", info).done(function(weather) {
+            cityName.html(" " + weather.city.name);
+            weatherBoxForecast(weather);
+        })
+    }
+
+
+    function weatherBoxForecast(weather) {
+        mapHolder.html("");
+        var daysArray = weather.list;
+        daysArray.forEach(function (day, i) {
+            var description = day.weather[0].description;
+            var highTemp = day.temp.max.toFixed(0);
+            var lowTemp = day.temp.min.toFixed(0);
+            var humidity = day.humidity;
+            var wind = day.speed.toFixed(0);
+            var date = weather.dt;
+            var icon = day.weather[0].icon;
+            var timestamp;
+
+            if (i == 0) {
+                timestamp = moment().format("dddd");
+            } else if (i == 1) {
+                timestamp = moment().add(1, "days").format("dddd");
+            } else {
+                timestamp = moment().add(2, "days").format("dddd");
+            }
+
+
+            var data = "<div class=\"dayDiv col-md-4\">" + timestamp + "<h3>" + highTemp + "&deg;/" + lowTemp + "&deg;</h3>" + "<p><img alt=\"weather icon\" src=\"http://openweathermap.org/img/w/" + icon + ".png\"></p><p><span class=\"heading\">" + main + ":</span> " + description + "</p><p><span class=\"heading\">Humidity:</span> " + humidity + "&#37;</p><p><span class=\"heading\">Wind:</span> " + wind + " mph</p></div>";
+            mapHolder.append(data);
+        })
+    }
+
+    enterLocation.on("click", function () {
+        city = $("#location").val();
+        geocodeAddress();
+        map.setZoom(9);
+    });
+
+    geocodeAddress();
+
+}));
+
