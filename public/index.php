7da762dcc8f1f2263b6ead3b30154f7fbf3e1e52
Chris Davila <cmd1603@gmail.com>
2016-09-26 15:21:10 -0500

    CSS changes to overall look of site

--- public/index.php
+++ public/index.php
@@ -26,17 +26,15 @@ extract(pageController());
 </head>
 <body>
     <?php require_once "navbar.php" ?>
-
-    <div class="slide-1">
-        <div class="overlay"></div>
-    </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-4 hero">
-                <hgroup>
-                    <h1 style="color: white">Hi!</h1>
-                    <h3>Welcome to my site!</h3>
-                </hgroup>
+            <div class="col-md-6" style="margin-top: 10%">
+                <h1 style="font-size: 64px">Hello!</h1>
+                <h3 style="font-size: 32px; margin-top: -10px; text-align: center">Welcome to my site!</h3>
+                <h4>My name is Chris Davila. I'm a web and UI developer based in San Antonio, TX. I have experience developing real-world projects in the LAMP stack and love learning new technologies in web development. Please get in touch with any questions you have.</h4>
+            </div>
+            <div class="col-md-6" style="margin-top: 10%">
+                <img src="/img/codeup_photo.jpeg" class="text-center img-responsive center-block" style="width=195px; height=253px; padding:1px; border:1px solid black;">
             </div>
         </div>
     </div>
