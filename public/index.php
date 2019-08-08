8b4d23f025480ae91ce04ba539b16814823592c9
Chris Davila <cmd1603@gmail.com>
2019-08-08 15:50:56 -0500

    Testing to see if push to warpspeed will work

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ extract(pageController());
             <div class="col-md-6" style="margin-top: 10%">
                 <h1 class="landing-page-font" style="font-size: 64px">Web Developer</h1>
                 <h3 class="landing-page-font" style="font-size: 32px; margin-top: -10px; text-align: center">Welcome to my site!</h3>
-                <h4 class="landing-page-font">My name is Chris Davila. I'm a web developer based in Dallas, TX. I have experience developing real-world web applications in the LAMP stack and love learning new technologies in web development. I am always looking for new projects to dabble in so please get in touch with any questions you may have.</h4>
+                <h4 class="landing-page-font">My name is Chris Davila. I'm a web developer based in Dallas, TX. I have experience developing real-world web applications in the LAMP stack and love learning new technologies in web development. I am always looking for new projects to dabble in so please get in touch with any questions you may have</h4>
             </div>
             <div class="col-md-6" style="margin-top: 10%">
                 <img src="/img/codeup_photo.jpeg" class="text-center img-responsive center-block" style="width=195px; height=253px; padding:1px; border:1px solid black;">
